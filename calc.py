f4212d4b4a9bcac3518310c1f16ed592a416ad05
dev1 <dev1@demo.lab>
2026-06-25 17:35:11 +0530

    Commit 3: Multiplication feature added

--- calc.py
+++ calc.py
@@ -2,4 +2,5 @@ n1 = int(input('First num: '))
 n2 = int(input('Second num: '))
 
 print(f"Addition is: {n1 + n2}")
-print(f"SUbtraction is {n1 - n2}")
+print(f"Subtraction is {n1 - n2}")
+print(f"Multiplication is {n1 * n2}")
